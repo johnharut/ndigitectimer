ffe4e6c3dfcb121a0ebcf28d948773c10a0c6cb4
John Harutyunyan <john.harutyunyan1995@gmail.com>
2017-04-03 17:25:25 +0400

    Added Card Button

--- public/js/timer.js
+++ public/js/timer.js
@@ -2,22 +2,38 @@ var Promise = TrelloPowerUp.Promise;
 
 var TIMER_ICON = 'https://cdn.glitch.com/a70126d2-0a54-42d9-b2bd-7f5d589fd3ff%2Ffavicon.png?1491218389939';
 
-TrelloPowerUp.initialize({
-  'board-buttons': function(t, options) {
-    return [{
-      icon: TIMER_ICON,
-      text: 'Add time',
-      url:"",
-      target: 'NDigitec'
-    }];
-  },
-  'show-settings': function(t, options){
-    return t.popup({
-      title: 'Settings',
-      url: './settings.html',
-      height: 180
-    });
-  }
+var getBadges = function (t) {
+    return t.cardButton
+}
 
-  //
+TrelloPowerUp.initialize({
+
+    'authorization-status': function (t, options) {
+        return new TrelloPowerUp.Promise(resolve({authorized: true }))
+    },
+
+    'card-buttons': function () {
+        return [{
+            icon: TIMER_ICON,
+            text: 'Add time',
+            url: 'http://www.ndigitec.com/',
+            target: ''
+        }];
+    },
+
+    'board-buttons': function (t, options) {
+        return [{
+            icon: TIMER_ICON,
+            text: 'Add time',
+            url: "",
+            target: 'NDigitec'
+        }];
+    },
+    'show-settings': function (t, options) {
+        return t.popup({
+            title: 'Settings',
+            url: './settings.html',
+            height: 180
+        });
+    }
 });
